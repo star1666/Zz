27af78e615991c4c5d7c8a2ae68b0e70242c74d9
z <79776453+Star8862@users.noreply.github.com>
2021-03-22 13:57:55 +0800

    Update unicom.js

--- commands/tasks/unicom/unicom.js
+++ commands/tasks/unicom/unicom.js
@@ -202,11 +202,6 @@ var start = async (params) => {
   }, taskOption)
 
   // 首页-游戏-娱乐中心-天天领取3G流量包
-  await scheduler.regTask('dailygameflow', async (request) => {
-    await require('./producGame').doGameFlowTask(request, options)
-  }, taskOption)
-
-  // 首页-积分查询-游戏任务
   await scheduler.regTask('dailygameIntegral', async (request) => {
     await require('./producGame').doGameIntegralTask(request, options)
   }, taskOption)
